58628545464ea06774eb7634e09d11423615f263
GBalatti9 <gas.balatti@gmail.com>
2024-01-14 12:43:53 -0300

    Setting up login styles

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,4 +1,4 @@
-import { MailOutline } from "@mui/icons-material"
+
 import { Typography } from "@mui/material"
 
 
@@ -6,7 +6,6 @@ export const JournalPage = () => {
     return (
         <>
         <Typography variant="h1"> JorunalPage </Typography>
-        <MailOutline />
         </>
     )
 }
