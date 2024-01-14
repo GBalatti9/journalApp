599e3a079d7e99fad61992e3fbb5bd4ad64e28bb
GBalatti9 <gas.balatti@gmail.com>
2024-01-14 16:02:42 -0300

    Skeleton of the project done

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,7 +1,9 @@
 
 
+import { IconButton } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
 import { NoteView, NothingSelectedView } from "../views";
+import { AddOutlined } from "@mui/icons-material";
 
 
 export const JournalPage = () => {
@@ -10,6 +12,19 @@ export const JournalPage = () => {
             {/* <NothingSelectedView /> */}
 
             <NoteView />
+
+            <IconButton
+                size="large"
+                sx={{
+                    color: 'white',
+                    backgroundColor: 'error.main',
+                    ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+                    position: 'fixed',
+                    right: 50,
+                    bottom: 50
+                }}>
+                    <AddOutlined sx={{ fontSize: 30 }} />
+            </IconButton>
         </JournalLayout>
     )
 }
