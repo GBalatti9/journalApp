745bee313f6d3a1d26f5ad614777978bc9b30226
GBalatti9 <gas.balatti@gmail.com>
2024-01-14 11:42:04 -0300

    Router and routes for login

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from "react-router-dom"
+
+export const AppRouter = () => {
+    return (
+        <Routes>
+
+            {/* Login y Registro */}
+            <Route />
+
+            {/* JournalApp */}
+            <Route />
+            
+        </Routes>
+    )
+}
