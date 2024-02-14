ff3faddbd8cbf7c724118075721de6bc6109b236
GBalatti9 <gas.balatti@gmail.com>
2024-02-14 08:59:20 -0300

    Configuration LoginPage functions and thunks

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -2,21 +2,44 @@ import { Google } from "@mui/icons-material"
 import { Button, Grid, TextField, Typography, Link } from "@mui/material"
 import { Link as RouterLink } from "react-router-dom"
 import { AuthLayout } from "../layout/AuthLayout"
+import { useForm } from "../../hooks"
 
 
 export const LoginPage = () => {
+
+
+    const { email, password, onInputChange } = useForm({
+        email: 'gas.balatti@gmail.com',
+        password: '123456'
+    });
+
+    const handleSubmit = ( e ) => {
+        e.preventDefault();
+
+        console.log({ email, password });
+
+    }
+
+    const handleGoogleSignIn = () => {
+        console.log('handle google singn in');
+    }
+
     return (
 
 
         <AuthLayout title='Login' >
-            <form>
+            <form onSubmit={ handleSubmit }>
                 <Grid container>
                     <Grid item xs={12} sx={{ mt: 2 }}>
                         <TextField
                             label='Email'
                             type="email"
                             placeholder="mail@gmail.com"
-                            fullWidth />
+                            fullWidth 
+                            name="email"
+                            value={ email }
+                            onChange={ onInputChange }
+                            />
                     </Grid>
 
                     <Grid item xs={12} sx={{ mt: 2 }}>
@@ -24,17 +47,25 @@ export const LoginPage = () => {
                             label='Contraseña'
                             type="password"
                             placeholder="Contraseña"
-                            fullWidth />
+                            fullWidth 
+                            name="password"
+                            value={ password }
+                            onChange={ onInputChange }
+                            />
                     </Grid>
 
                     <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
                         <Grid item xs={12} sm={6}>
-                            <Button variant="contained" fullWidth>
+                            <Button type="submit" variant="contained" fullWidth>
                                 Login
                             </Button>
                         </Grid>
                         <Grid item xs={12} sm={6}>
-                            <Button variant="contained" fullWidth>
+                            <Button 
+                                onClick = { handleGoogleSignIn } 
+                                variant = "contained" 
+                                fullWidth
+                                >
                                 <Google />
                                 <Typography sx={{ ml: 1 }} variant="p"> Google </Typography>
                             </Button>
