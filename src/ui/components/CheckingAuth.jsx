87cde577fe55613c8543168e995fe221965c31d1
GBalatti9 <gas.balatti@gmail.com>
2024-02-15 09:57:00 -0300

    Protecting routes with toolkit

--- src/ui/components/CheckingAuth.jsx
+++ src/ui/components/CheckingAuth.jsx
@@ -13,7 +13,6 @@ export const CheckingAuth = ({ children, title = '' }) => {
             >
 
             <Grid item
-                direction="row"
                 justifyContent="center"
                 >
                     
