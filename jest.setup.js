fd16b1ccd1583e56413962fc435249748aea307b
GBalatti9 <gas.balatti@gmail.com>
2024-02-21 22:34:48 -0300

    Testing

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,11 @@
 import 'whatwg-fetch';
 import 'setimmediate';
+import { getEnvironments } from './src/helpers';
+
+required('dotenv').config({
+    path: 'env.test',
+})
+
+jest.mock('./src/helpers', () => ({
+    getEnvironments: () => ({ ...process.env }),
+}))
